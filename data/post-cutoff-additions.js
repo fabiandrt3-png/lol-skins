5a6def78373f36fbf1b5828114d9db9f56824513
fabiandrt3-png <fabiandaret3@gmail.com>
2026-09-10 12:46:37 +0200

    Add post-cutoff PC and Wild Rift skin catalog

---
+++ data/post-cutoff-additions.js
@@ -0,0 +1,116 @@
+const WIKI = "https://wiki.leagueoflegends.com/en-us/Special:Redirect/file/";
+
+function slugify(value = "") {
+  return String(value).normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-+|-+$/g, "");
+}
+
+function wikiFile(champion, descriptor, suffix = "") {
+  const prefix = String(champion).replaceAll(" ", "_");
+  return `${WIKI}${encodeURIComponent(`${prefix}_${descriptor}Skin${suffix}.jpg`)}`;
+}
+
+function wr(champ, skin, releaseDate, descriptor, extraFallbacks = []) {
+  const standard = wikiFile(champ, descriptor, "_WR");
+  const hd = wikiFile(champ, descriptor, "_WR_HD");
+  const pcStandard = wikiFile(champ, descriptor, "");
+  const pcHd = wikiFile(champ, descriptor, "_HD");
+  return {
+    id: `${slugify(champ)}::${slugify(skin)}::wild-rift`, champ, skin, type: "Wild Rift", releaseDate,
+    image: standard,
+    fallbacks: [...extraFallbacks, pcStandard, hd],
+    fullImage: hd,
+    fullHdFallbacks: [pcHd, standard, pcStandard, ...extraFallbacks],
+  };
+}
+
+function pc(champ, skin, releaseDate, alias, number, descriptor) {
+  const skinFolder = `skin${String(number).padStart(2, "0")}`;
+  const root = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/characters/${alias}/skins/${skinFolder}/images/${alias}_splash_`;
+  const centered = `${root}centered_${number}.jpg`;
+  const uncentered = `${root}uncentered_${number}.jpg`;
+  return {
+    id: `${slugify(champ)}::${slugify(skin)}::pc`, champ, skin, type: "PC", releaseDate,
+    image: centered,
+    fallbacks: [uncentered],
+    fullImage: wikiFile(champ, descriptor, "_HD"),
+    fullHdFallbacks: [uncentered, centered],
+  };
+}
+
+export const postCutoffAdditions = [
+  pc("Jayce", "Petals of Spring Jayce", "2026-02-19", "jayce", 38, "PetalsofSpring"),
+  pc("Katarina", "Petals of Spring Katarina", "2026-02-19", "katarina", 70, "PetalsofSpring"),
+  pc("Lillia", "Petals of Spring Lillia", "2026-02-19", "lillia", 46, "PetalsofSpring"),
+  pc("Yasuo", "Petals of Spring Yasuo", "2026-02-19", "yasuo", 88, "PetalsofSpring"),
+
+  wr("Vayne", "Dragonmancer Vayne", "2026-03-06", "Dragonmancer"),
+  wr("Fiora", "Dragonmancer Fiora", "2026-03-06", "Dragonmancer"),
+  wr("Nilah", "Dragonmancer Nilah", "2026-03-06", "Dragonmancer"),
+  wr("Thresh", "Janitor Thresh", "2026-03-08", "Janitor"),
+  wr("Mel", "Love Confession Mel", "2026-03-13", "LoveConfession"),
+  wr("Jayce", "Love Confession Jayce", "2026-03-13", "LoveConfession"),
+
+  wr("Master Yi", "PsyOps Master Yi (Special Edition)", "2026-04-09", "PsyOpsSpecialEdition"),
+  wr("Viego", "Soul Fighter Viego", "2026-04-09", "SoulFighter"),
+  wr("Senna", "Soul Fighter Senna", "2026-04-09", "SoulFighter"),
+  wr("Ambessa", "Soul Fighter Ambessa", "2026-04-09", "SoulFighter"),
+  wr("Viktor", "Soul Fighter Viktor", "2026-04-09", "SoulFighter"),
+  wr("Varus", "Soul Fighter Varus", "2026-04-09", "SoulFighter"),
+  wr("Varus", "Ascended Soul Fighter Varus", "2026-04-09", "AscendedSoulFighter"),
+  wr("Jax", "Glorious Eminence Jax", "2026-04-10", "GloriousEminence"),
+
+  wr("Kai'Sa", "Neon Daredevil Kai'Sa", "2026-04-30", "NeonDaredevil"),
+  wr("Hecarim", "Neon Daredevil Hecarim", "2026-04-30", "NeonDaredevil"),
+  wr("Irelia", "Neon Daredevil Irelia", "2026-04-30", "NeonDaredevil"),
+  wr("Aurora", "Neon Daredevil Aurora", "2026-04-30", "NeonDaredevil"),
+  wr("Zed", "Neon Daredevil Zed", "2026-04-30", "NeonDaredevil"),
+  wr("Gragas", "Neon Daredevil Gragas", "2026-04-30", "NeonDaredevil"),
+  wr("Kai'Sa", "Prestige Select Neon Daredevil Kai'Sa", "2026-04-30", "PrestigeSelectNeonDaredevil"),
+
+  wr("Taliyah", "Pool Party Taliyah", "2026-05-15", "PoolParty"),
+  wr("Norra", "Cafe Cuties Norra", "2026-05-20", "CafeCuties"),
+  wr("Master Yi", "Neon Daredevil Master Yi: Origin", "2026-05-22", "NeonDaredevilOrigin"),
+  wr("Master Yi", "Neon Daredevil Master Yi: Velocity", "2026-05-22", "NeonDaredevilVelocity"),
+  wr("Master Yi", "Neon Daredevil Master Yi: Megacorps", "2026-05-22", "NeonDaredevilMegacorps"),
+  wr("Master Yi", "Neon Daredevil Master Yi: Neon Speed", "2026-05-22", "NeonDaredevilNeonSpeed"),
+  wr("Master Yi", "Neon Daredevil Master Yi: Swiftbladez", "2026-05-22", "NeonDaredevilSwiftbladez"),
+  wr("Ornn", "Choo-Choo Ornn", "2026-06-06", "ChooChoo"),
+  wr("Sion", "Plant Protector Sion", "2026-06-12", "PlantProtector"),
+  wr("Samira", "Ashen Avenger Samira", "2026-06-12", "AshenAvenger"),
+
+  wr("Yunara", "Weather Entity Yunara", "2026-07-09", "WeatherEntity"),
+  wr("Poppy", "Weather Entity Poppy", "2026-07-09", "WeatherEntity"),
+  wr("Nocturne", "Weather Entity Nocturne", "2026-07-09", "WeatherEntity"),
+  wr("Ashe", "Weather Entity Ashe", "2026-07-09", "WeatherEntity"),
+  wr("Vladimir", "Prestige Weather Entity Vladimir", "2026-07-09", "PrestigeWeatherEntity"),
+  wr("Ryze", "Movie Director Ryze", "2026-07-09", "MovieDirector"),
+  wr("Ryze", "Ascended Movie Director Ryze", "2026-07-09", "AscendedMovieDirector"),
+  wr("Skarner", "Cosmic Sting Skarner", "2026-07-09", "CosmicSting"),
+  wr("Renekton", "Glorious Eminence Renekton", "2026-07-10", "GloriousEminence"),
+  wr("Lulu", "Purple Garlic Lulu", "2026-07-10", "PurpleGarlic"),
+  wr("Galio", "Garden Party Galio", "2026-07-17", "GardenParty"),
+
+  wr("Syndra", "Island Vacation Syndra", "2026-07-24", "IslandVacation"),
+  wr("Gwen", "Island Vacation Gwen", "2026-07-24", "IslandVacation"),
+  wr("Karma", "Island Vacation Karma", "2026-07-24", "IslandVacation"),
+  wr("Morgana", "Island Vacation Morgana", "2026-07-24", "IslandVacation"),
+  wr("Katarina", "Summer Party Katarina (Exquisite Edition)", "2026-07-24", "SummerPartyExquisiteEdition"),
+
+  wr("Annie", "Annie-Versary", "2026-08-07", "Annie-Versary"),
+  wr("Leona", "Crystal Rose Leona", "2026-08-14", "CrystalRose"),
+  wr("Diana", "Crystal Rose Diana", "2026-08-14", "CrystalRose"),
+  wr("Nami", "Crystal Rose Nami", "2026-08-14", "CrystalRose"),
+  wr("Miss Fortune", "Crystal Rose Miss Fortune", "2026-08-14", "CrystalRose"),
+  wr("Yasuo", "Crystal Rose Yasuo", "2026-08-14", "CrystalRose"),
+  wr("Akali", "Prestige Crystal Rose Akali", "2026-08-14", "PrestigeCrystalRose"),
+  wr("Sona", "Prestige Crystal Rose Sona", "2026-08-14", "PrestigeCrystalRose"),
+  wr("Skarner", "Scorpio Deity Skarner", "2026-08-27", "ScorpioDeity"),
+  wr("Skarner", "Scorpio Deity Ancient Wisdom Skarner", "2026-08-27", "ScorpioDeityAncientWisdom"),
+  wr("Amumu", "Bookworm Amumu", "2026-08-28", "Bookworm"),
+  wr("Nautilus", "Trash Diver Nautilus", "2026-08-28", "TrashDiver"),
+
+  wr("Kayn", "Odyssey Kayn", "2026-09-04", "Odyssey"),
+  wr("Lucian", "PROJECT: Lucian (Exquisite Edition)", "2026-09-04", "PROJECTExquisiteEdition"),
+  wr("Pyke", "PROJECT: Pyke (Exquisite Edition)", "2026-09-04", "PROJECTExquisiteEdition"),
+  wr("Varus", "Elf Forest Varus", "2026-09-10", "ElfForest"),
+];
